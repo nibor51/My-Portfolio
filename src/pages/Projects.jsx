9083a960ce878c4cbd2b411d9bea15e7285e189c
Nibor51 <jonval.robin@gmail.com>
2022-07-02 14:35:33 +0200

    create file for storage project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,26 +1,4 @@
-const projectList = [
-  {
-    name: "Reims Quiz",
-    description:
-      "Répondez à un petit Quiz sur la ville de Reims afin de la découvrir à travers des anecdotes la concernent. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, harum? Blanditiis non sint, rerum quisquam earum iusto? Tempore aspernatur molestiae voluptatum blanditiis, beatae corrupti provident, recusandae iure quia neque rerum?",
-    image: "https://source.unsplash.com/random/400x200",
-    url: "https://nibor51.github.io/scaling-journey/docs",
-  },
-  {
-    name: "Baker Street Fighter",
-    description:
-      "Venez à deux sur votre ordinateur pendant votre pause, pour vous battre dans se petit jeu de boxe situer dans l'univers de Sherlock Holmes 8bit ! Site web dynamique réalisé à la Wild Code School.",
-    image: "https://source.unsplash.com/random/400x200",
-    url: "https://github.com/WildCodeSchool/reims-php-2109-project2-baker-street-fighter",
-  },
-  {
-    name: "Motorbox",
-    description:
-      "Motorbox est née dans l'esprit de Baptiste jesaispluscomment, le but de cette aplication est de réliaser le suivi d'entretien de votre moto. En effet beaucoup de pièces necessite un entretien constant. Site web dynamique réalisé en partenariat avec une startup incubé chez Innovact",
-    image: "https://source.unsplash.com/random/400x200",
-    url: "https://github.com/WildCodeSchool/reims-202109-php-project3-motorbox",
-  },
-];
+import projects from "../data/projects.json";
 
 export default function Projects() {
   return (
@@ -29,7 +7,7 @@ export default function Projects() {
         <h1>Projects</h1>
         <p>This is the project page.</p>
         <ul>
-          {projectList.map((project) => (
+          {projects.map((project) => (
             <li key={project.name}>
               <h2>{project.name}</h2>
               <p>{project.description}</p>
