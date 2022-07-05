554c6efa0b24acd312518e70e31401242bca93b5
Nibor51 <jonval.robin@gmail.com>
2022-07-05 11:12:02 +0200

    use modal for project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
 import ProjectType from "../prop-types/ProjectType";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 export default function Project({ project }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <>
-      <li>
-        <h2>{project.name}</h2>
-        <p>{project.description}</p>
-        <img src={project.image} alt={project.name} />
-        <a href={project.url}>{project.url}</a>
-      </li>
+    <li>
+      <Button type="button" onClick={handleOpen}>
+        <figure>
+          <img src={project.image} alt={project.name} />
+          <figcaption>{project.name}</figcaption>
+        </figure>
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box sx={style}>
+          <h2 id="modal-title">{project.name}</h2>
+          <img src={project.image} alt={project.name} />
+          <p id="modal-description">{project.description}</p>
+          <a href={project.url}>{project.url}</a>
+        </Box>
+      </Modal>
       <style jsx="true">
         {`
           #project li {
@@ -30,7 +63,7 @@ export default function Project({ project }) {
           }
         `}
       </style>
-    </>
+    </li>
   );
 }
 
